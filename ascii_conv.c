a72fdba524201a5e88506e97695f691ef1861abf
Connor Northway <32226655+cnorthway@users.noreply.github.com>
2022-03-08 02:24:10 -0500

    clang-format

--- ascii_conv.c
+++ ascii_conv.c
@@ -1,20 +1,18 @@
 #include <assert.h>
 #include <fcntl.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <sys/types.h>
+#include <stdlib.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <syslog.h>
 #include <unistd.h>
 
-int
-main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
   assert(argc == 3 && "usage: ascii_conv $input_fifo $output_fifo");
   if (mkfifo(argv[1], 0666) == -1) {
     syslog(LOG_ERR, "creating ascii input fifo failed (%m)");
   }
-  FILE* in_file = fopen(argv[1], "r");
+  FILE *in_file = fopen(argv[1], "r");
   if (!in_file) {
     syslog(LOG_ERR, "opening ascii input fifo failed (%m)");
   }
@@ -22,14 +20,11 @@ main(int argc, char** argv)
   if (out_fd == -1) {
     syslog(LOG_ERR, "opening bin output fifo failed (%m)");
   }
-  while (1)
-  {
+  while (1) {
     int val;
     int ret = fscanf(in_file, "%i", &val);
-    if (ret == EOF)
-    {
-      if (ferror(in_file) != 0)
-      {
+    if (ret == EOF) {
+      if (ferror(in_file) != 0) {
         // not end of file (expected), we would like to know what's goin on
         syslog(LOG_INFO, "ascii integer read error");
       }
