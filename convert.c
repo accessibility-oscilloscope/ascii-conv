6838eaeb617b68bd92f011895617d7b5017fac70
Nick Fantasia <fantasia.n@northeastern.edu>
2022-03-07 21:13:51 -0500

    Format fixing

--- convert.c
+++ convert.c
@@ -3,13 +3,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <assert.h>
- #include <fcntl.h>
-
+#include <fcntl.h>
 
 int main(int ac, char *av[])
 {
     assert(ac == 3 && "usage: ./convert in-fifo out-fifo");
-    int result, val1, val2, buffset, offset1, offset2;
+    int result = 0, val1 = 0, val2 = 0, buffset = 0, offset1 = 0, offset2 = 0;
     char *buffer;
     char arr1[100];
     char arr2[100];
@@ -27,14 +26,14 @@ int main(int ac, char *av[])
         perror("Unable to allocate buffer");
         exit(1);
     }
-   
+
     const int fd = open(in_fifo, O_RDONLY);
     const int fd_out = open(out_fifo, O_WRONLY);
 
     while (1)
     {
-        result = read(fd, (void *) (&buffer[buffset]), 1);
-        
+        result = read(fd, (void *)(&buffer[buffset]), 1);
+
         if (result != 1)
         {
             break;
@@ -55,14 +54,12 @@ int main(int ac, char *av[])
         }
         buffset++;
     }
-    
 
     val1 = atoi(arr1);
     val2 = atoi(arr2);
     output[0] = val1;
     output[1] = val2;
 
-    //printf("%d %d", output[0], output[1]);
     write(fd_out, &output, 2);
     free(buffer);
 
