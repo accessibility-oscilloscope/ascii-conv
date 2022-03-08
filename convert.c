e2d19a1940eb6741270b203a6e788d7d47c3532c
Nick Fantasia <fantasia.n@northeastern.edu>
2022-03-07 20:26:26 -0500

    Fixes all around

--- convert.c
+++ convert.c
@@ -1,49 +1,62 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <fcntl.h> 
 #include <unistd.h>
 
-int main(int argc, char** argv) {
 
-int i=1,j,tmp, val,remainder,quotient,fd;
-char hexadecimalNumber[100];
-
-char *buffer;
-size_t size = 32;
-
-buffer = (char *)malloc(size * sizeof(char));
-if( buffer == NULL)
+int main(int argc, char **argv)
 {
-    perror("Unable to allocate buffer");
-    exit(1);
-}
 
-//printf("Type a number to convert: ");
-//getline(&buffer,&size,stdin);
+    int result, val1, val2, buffset, offset1, offset2;
+    char *buffer;
+    char arr1[100];
+    char arr2[100];
 
-//fd = open("test.txt", O_RDONLY);
+    size_t size = 32;
+    u_int8_t output[2];
+    bool first = true;
 
-read(STDIN_FILENO, buffer, size);
+    buffer = (char *)malloc(size * sizeof(char));
+    if (buffer == NULL)
+    {
+        perror("Unable to allocate buffer");
+        exit(1);
+    }
+   
+    while (1)
+    {
+        result = read(STDIN_FILENO, (void *) (&buffer[buffset]), 1);
+        
+        if (result != 1)
+        {
+            break;
+        }
+        if (buffer[buffset] == '\n')
+        {
+            first = false;
+        }
+        if (first)
+        {
+            arr1[offset1] = buffer[buffset];
+            offset1++;
+        }
+        else
+        {
+            arr2[offset2] = buffer[buffset];
+            offset2++;
+        }
+        buffset++;
+    }
+    
 
-val = atoi(buffer);
+    val1 = atoi(arr1);
+    val2 = atoi(arr2);
+    output[0] = val1;
+    output[1] = val2;
 
-quotient = val;
+    //printf("%d %d", output[0], output[1]);
+    write(STDOUT_FILENO, &output, 2);
+    free(buffer);
 
-while(quotient!=0) {
-	tmp = quotient % 16;
-	//To convert integer into character
-	if( tmp < 10)
-		        tmp =tmp + 48; else
-		        tmp = tmp + 55;
-	hexadecimalNumber[i++]= tmp;
-	quotient = quotient / 16;
-	}
-
-	for (j = i -1 ;j> 0;j--)
-	      printf("%c",hexadecimalNumber[j]);
-
-free(buffer);
-return 0;
-
+    return 0;
 }
